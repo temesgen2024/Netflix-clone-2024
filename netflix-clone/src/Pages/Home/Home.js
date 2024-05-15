e0247dc7acb59c4b37578dec62cef4bd67cd97e5
temesgen2024 <162815146+temesgen2024@users.noreply.github.com>
2024-05-14 22:09:31 -0400

    header and footer

---
+++ netflix-clone/src/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../Components/Header";
+
+import Footer from '../../Components/Footer/Footer'
+
+function Home() {
+  return (
+    <div>
+      {/* <Header /> */}
+      <Header />
+      <Footer/>
+    </div>
+  );
+}
+
+export default Home;
