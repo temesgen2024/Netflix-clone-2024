4a14e7e1201b78ae245d75841d1004c46912011d
temesgen2024 <162815146+temesgen2024@users.noreply.github.com>
2024-05-15 21:43:48 -0400

    Banner

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../../Components/Header";
 
 import Footer from '../../Components/Footer/Footer'
+import Banner from "../../Components/Banner/Banner";
 
 function Home() {
   return (
     <div>
       {/* <Header /> */}
       <Header />
+      <Banner/>
       <Footer/>
       
     </div>
